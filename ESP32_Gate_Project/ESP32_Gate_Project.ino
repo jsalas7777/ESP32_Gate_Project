ac4e597428bcac4181037b4d7f9e24277294b06f
jose <jsalas777@proton.me>
2026-09-24 16:11:21 -0600

    gates

--- ESP32_Gate_Project/ESP32_Gate_Project.ino
+++ ESP32_Gate_Project/ESP32_Gate_Project.ino
@@ -1,11 +1,32 @@
 const int LED_PIN = 25;
-const int EXTRA_PINS[] = {26, 27, 28};
-const int EXTRA_PINS_COUNT = sizeof(EXTRA_PINS) / sizeof(EXTRA_PINS[0]);
+const int GATE_1 = 26;
+const int GATE_2 = 27;
+const int GATE_3 = 14;
+const int GATES[] = {GATE_1, GATE_2, GATE_3};
+const char *GATE_NAMES[] = {"gate_1", "gate_2", "gate_3"};
+const int GATE_COUNT = sizeof(GATES) / sizeof(GATES[0]);
+const unsigned long GATE_ON_MS = 1000;
+const unsigned long GATE_OFF_MS = 1000;
 const unsigned long LOG_INTERVAL_MS = 2000;
 const int BOOT_BLINKS = 3;
 const int BLINK_MS = 200;
 
 unsigned long lastLog = 0;
+unsigned long lastStep = 0;
+int activeGate = 0;
+bool gateOn = false;
+
+void gateOnNow(int index) {
+  digitalWrite(GATES[index], HIGH);
+  gateOn = true;
+  Serial.printf("[GATE] %s ON (GPIO%d)\n", GATE_NAMES[index], GATES[index]);
+}
+
+void gateOffNow(int index) {
+  digitalWrite(GATES[index], LOW);
+  gateOn = false;
+  Serial.printf("[GATE] %s OFF (GPIO%d)\n", GATE_NAMES[index], GATES[index]);
+}
 
 const char *resetReasonName(esp_reset_reason_t r) {
   switch (r) {
@@ -25,9 +46,9 @@ const char *resetReasonName(esp_reset_reason_t r) {
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
-  for (int i = 0; i < EXTRA_PINS_COUNT; i++) {
-    pinMode(EXTRA_PINS[i], OUTPUT);
-    digitalWrite(EXTRA_PINS[i], HIGH);
+  for (int i = 0; i < GATE_COUNT; i++) {
+    pinMode(GATES[i], OUTPUT);
+    digitalWrite(GATES[i], LOW);
   }
 
   Serial.printf("[BOOT] ESP32 started, reset reason: %s\n", resetReasonName(esp_reset_reason()));
@@ -41,9 +62,22 @@ void setup() {
 
   digitalWrite(LED_PIN, HIGH);
   Serial.println("[BOOT] blink done, GPIO25 ON");
+
+  gateOnNow(activeGate);
+  lastStep = millis();
 }
 
 void loop() {
+  // gate_1 ON -> OFF -> gate_2 ON -> OFF -> gate_3 ON -> OFF -> repeat
+  if (gateOn && millis() - lastStep >= GATE_ON_MS) {
+    lastStep = millis();
+    gateOffNow(activeGate);
+  } else if (!gateOn && millis() - lastStep >= GATE_OFF_MS) {
+    lastStep = millis();
+    activeGate = (activeGate + 1) % GATE_COUNT;
+    gateOnNow(activeGate);
+  }
+
   if (millis() - lastLog >= LOG_INTERVAL_MS) {
     lastLog = millis();
     Serial.printf("[OK] running, uptime=%lus, GPIO25=%d\n", millis() / 1000, digitalRead(LED_PIN));
